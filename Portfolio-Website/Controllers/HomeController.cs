616769ab94e5e2c2aaa5475e4d1c99b10f765bcd
anil maurya <138668871+webdev14buzz@users.noreply.github.com>
2024-05-19 15:25:27 +0530

    Add project files.

---
+++ Portfolio-Website/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio_Website.Models;
+using System.Diagnostics;
+
+namespace Portfolio_Website.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _configuration = config;
+        }
+
+        public IActionResult Index()
+        {
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
